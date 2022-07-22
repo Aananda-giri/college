65dbbdda89bd5939291185e513a783af3f5da18d
nathan <nathan@localhost.localdomain>
2022-07-22 08:50:44 +0545

    database project

--- project/models.py
+++ project/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Queries(models.Model):
+    query = models.CharField(max_length=100)
+    def print_queries():
+        results = Queries.objects.raw('SELECT * FROM project_queries')
+        print(f'\n ---{results}--- \n')
+        return results
+        pass
+
--- project/views.py
+++ project/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Queries
 
 # Create your views here.
 
@@ -7,6 +8,15 @@ def index(request):
         # get the nick name from the client side.
         search_term = request.GET.get("search_query", None)
         if search_term != None:
+            print(f'\n ------{search_term}------\n')
+            try:
+                for i in Queries.objects.raw('INSERT INTO project_queries(\'query\') values(\'%s\');' % search_term):
+                    print(i)
+            except:
+                pass
+            
             # save to database
-            pass
-    return render(request, 'project/index.html')
+        
+    data = [i for i in Queries.objects.raw('SELECT * FROM project_queries')]
+    print(data)
+    return render(request, 'project/index.html', {'data': data})
